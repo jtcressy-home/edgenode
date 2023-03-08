f96ea94f757a16caf14edcd32e75531806f12c09
Joel Cressy <joel.cressy@dottid.com>
2023-03-08 14:26:56 -0600

    fix: finishing touches and fixes (#1)

--- scripts/cloudinit/00-upgrade-microk8s.sh
+++ scripts/cloudinit/00-upgrade-microk8s.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -xe
 
 # Check if installed version is same as the snap version and if yes skip
-installedVersion=$(snap list microk8s |grep microk8s | awk '{ print $2 }')
-packageVersion=$(snap info /opt/microk8s/snaps/microk8s.snap |grep "version:" | awk '{ print $2 }')
-if [ $installedVersion  !=  $packageVersion ]; then
-  echo "Diff k8s version... upgrading"
-  /opt/microk8s/scripts/00-install-microk8s.sh
-else
-  echo "Same Version...not upgrading"
-fi
+# installedVersion=$(snap list microk8s |grep microk8s | awk '{ print $2 }')
+# packageVersion=$(snap info /opt/microk8s/snaps/microk8s.snap |grep "version:" | awk '{ print $2 }')
+# if [ $installedVersion  !=  $packageVersion ]; then
+#   echo "Diff k8s version... upgrading"
+#   /opt/microk8s/scripts/00-install-microk8s.sh
+# else
+#   echo "Same Version...not upgrading"
+# fi
+until systemctl is-active snapd.seeded.service --quiet; do
+  echo "Wait until snapd is fully seeded..."
+  sleep 5
+done
+snap list microk8s && snap refresh microk8s
+microk8s status --wait-ready
